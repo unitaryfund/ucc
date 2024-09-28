4fdc321a1f84664d440b44838e389b06ee5b1820
Misty-W <82074193+Misty-W@users.noreply.github.com>
2024-09-27 17:07:23 -0700

    Update QFT unit test; temporarily add QFT notebook

--- ucc/__init__.py
+++ ucc/__init__.py
@@ -1,3 +1,3 @@
 from .transpilers import UCCTranspiler
 from .compile import compile, supported_circuit_formats
-from .custom_cx import CXCancellation
+from .transpiler_passes.custom_cx import CXCancellation
