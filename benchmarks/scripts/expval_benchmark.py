c1989bac58f6281227b18b13715db3ac3b2d6afb
nate stemen <nate@unitary.fund>
2024-11-26 20:30:07 -0800

    make data pipeline more functional

--- benchmarks/scripts/expval_benchmark.py
+++ benchmarks/scripts/expval_benchmark.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 import cirq
 import pytket
+import qiskit
 from cirq.contrib.qasm_import import circuit_from_qasm
 from qiskit import qasm2
 from qiskit.quantum_info import Operator, Statevector
@@ -17,80 +18,83 @@ from ucc import compile as ucc_compile
 with open("../circuits/qasm2/ucc/prep_select_N10_ghz.qasm") as f:
     qasm_string = f.read()
 
-qiskit_circuit = qasm2.loads(qasm_string)
-qiskit_circuit.save_density_matrix()
 
-ucc_compiled = ucc_compile(qasm_string, return_format="qiskit")
-ucc_compiled.save_density_matrix()
+def generate_compiled_circuits(qasm: str) -> dict[str, qiskit.QuantumCircuit]:
+    """Compiles the circuit represented in a QASM string using different
+    compilers before converting them all to Qiskit circuits.
 
-cirq_compiled = cirq.qasm(cirq_compile(circuit_from_qasm(qasm_string)))
-cirq_compiled = qasm2.loads(cirq_compiled)
-cirq_compiled.save_density_matrix()
+    Args:
+        qasm: The QASM string representing the quantum circuit to be compiled.
 
-qiskit_compiled = qiskit_compile(qasm2.loads(qasm_string))
-qiskit_compiled.save_density_matrix()
+    Returns:
+        A dictionary containing the compiled quantum circuits with keys:
+        uncompiled, ucc, cirq, qiskit, pytket.
+    """
+    uncompiled_circuit = qasm2.loads(qasm)
+    qiskit_compiled = qiskit_compile(uncompiled_circuit)
+    uncompiled_circuit.save_density_matrix()
+    qiskit_compiled.save_density_matrix()
 
-pytket_compiled = pytket.qasm.circuit_to_qasm_str(
-    pytket_compile(pytket.qasm.circuit_from_qasm_str(qasm_string))
-)
-pytket_compiled = qasm2.loads(pytket_compiled)
-pytket_compiled.save_density_matrix()
+    ucc_compiled = ucc_compile(qasm, return_format="qiskit")
+    ucc_compiled.save_density_matrix()
 
-depolarizing_noise = NoiseModel()
-error = depolarizing_error(0.03, 1)
-two_qubit_error = depolarizing_error(0.05, 2)
-depolarizing_noise.add_all_qubit_quantum_error(error, ["u1", "u2", "u3"])
-depolarizing_noise.add_all_qubit_quantum_error(two_qubit_error, ["cx"])
+    cirq_compiled = cirq.qasm(cirq_compile(circuit_from_qasm(qasm)))
+    cirq_compiled_qiskit = qasm2.loads(cirq_compiled)
+    cirq_compiled_qiskit.save_density_matrix()
 
-simulator = AerSimulator(method="density_matrix", noise_model=depolarizing_noise)
+    pytket_compiled = pytket.qasm.circuit_to_qasm_str(
+        pytket_compile(pytket.qasm.circuit_from_qasm_str(qasm_string))
+    )
+    pytket_compiled_qiskit = qasm2.loads(pytket_compiled)
+    pytket_compiled_qiskit.save_density_matrix()
+
+    return {
+        "uncompiled": uncompiled_circuit,
+        "ucc": ucc_compiled,
+        "cirq": cirq_compiled_qiskit,
+        "qiskit": qiskit_compiled,
+        "pytket": pytket_compiled_qiskit,
+    }
 
 
-uncompiled_dm = simulator.run(qiskit_circuit).result().data()["density_matrix"]
-ucc_dm = simulator.run(ucc_compiled).result().data()["density_matrix"]
-cirq_dm = simulator.run(cirq_compiled).result().data()["density_matrix"]
-qiskit_dm = simulator.run(qiskit_compiled).result().data()["density_matrix"]
-pytket_dm = simulator.run((pytket_compiled)).result().data()["density_matrix"]
+def simulate_density_matrix(circuit: qiskit.QuantumCircuit) -> np.ndarray:
+    depolarizing_noise = NoiseModel()
+    error = depolarizing_error(0.03, 1)
+    two_qubit_error = depolarizing_error(0.05, 2)
+    depolarizing_noise.add_all_qubit_quantum_error(error, ["u1", "u2", "u3"])
+    depolarizing_noise.add_all_qubit_quantum_error(two_qubit_error, ["cx"])
+
+    simulator = AerSimulator(method="density_matrix", noise_model=depolarizing_noise)
+    return simulator.run(circuit).result().data()["density_matrix"]
+
+
+density_matrices = {
+    compiler: simulate_density_matrix(circuit)
+    for compiler, circuit in generate_compiled_circuits(qasm_string).items()
+}
+
 
 observable = Operator.from_label("ZZZZZZZZZZ")
 
-uncompiled_expval = np.real_if_close(uncompiled_dm.expectation_value(observable)).item()
-ucc_expval = np.real_if_close(ucc_dm.expectation_value(observable)).item()
-cirq_expval = np.real_if_close(cirq_dm.expectation_value(observable)).item()
-qiskit_expval = np.real_if_close(qiskit_dm.expectation_value(observable)).item()
-pytket_expval = np.real_if_close(pytket_dm.expectation_value(observable)).item()
+expectation_values = {
+    compiler: np.real_if_close(dm.expectation_value(observable)).item()
+    for compiler, dm in density_matrices.items()
+}
+
 
 ideal_circuit = qasm2.loads(qasm_string)
 ideal_state = Statevector.from_instruction(ideal_circuit)
 ideal_expval = np.real_if_close(ideal_state.expectation_value(observable)).item()
 
 results = {
-    "ideal": ideal_expval,
-    "uncompiled": {
-        "expval": uncompiled_expval,
-        "abs_error": abs(ideal_expval - uncompiled_expval),
-        "relative_error": abs(ideal_expval - uncompiled_expval) / abs(ideal_expval),
-    },
-    "ucc": {
-        "expval": ucc_expval,
-        "abs_error": abs(ideal_expval - ucc_expval),
-        "relative_error": abs(ideal_expval - ucc_expval) / abs(ideal_expval),
-    },
-    "cirq": {
-        "expval": cirq_expval,
-        "abs_error": abs(ideal_expval - cirq_expval),
-        "relative_error": abs(ideal_expval - cirq_expval) / abs(ideal_expval),
-    },
-    "qiskit": {
-        "expval": qiskit_expval,
-        "abs_error": abs(ideal_expval - qiskit_expval),
-        "relative_error": abs(ideal_expval - qiskit_expval) / abs(ideal_expval),
-    },
-    "pytket": {
-        "expval": pytket_expval,
-        "abs_error": abs(ideal_expval - pytket_expval),
-        "relative_error": abs(ideal_expval - pytket_expval) / abs(ideal_expval),
-    },
+    compiler: {
+        "expval": expval,
+        "absoluate_error": abs(ideal_expval - expval),
+        "relative_error": abs(ideal_expval - expval) / abs(ideal_expval),
+    }
+    for compiler, expval in expectation_values.items()
 }
+results["ideal"] = ideal_expval
 print(results)
 
 filename = f"expval-results_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.json"
