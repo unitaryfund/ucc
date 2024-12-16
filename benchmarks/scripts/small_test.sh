ee42c6dd98cfa360c57c6a762da62b464ffa2a44
jordandsullivan <jordan@unitary.fund>
2024-12-16 10:47:09 -0800

    Generalize small_test shell script

--- benchmarks/scripts/small_test.sh
+++ benchmarks/scripts/small_test.sh
@@ -1,1 +1,12 @@
-python ./benchmarks/scripts/benchmark_script.py ./benchmarks/circuits/qasm2/benchpress/square_heisenberg_N100_basis_rz_rx_ry_cx.qasm ucc ./benchmarks/results
+QASM_FILE="benchpress/square_heisenberg_N100_basis_rz_rx_ry_cx.qasm"
+COMPILER="ucc"
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+RESULTS_FOLDER="$SCRIPT_DIR/../results/test_data"
+mkdir -p "$RESULTS_FOLDER"
+QASM_FOLDER="$SCRIPT_DIR/../circuits/qasm2/"
+full_qasm_file="$QASM_FOLDER/$QASM_FILE"
+
+command="python3 $SCRIPT_DIR/benchmark_script.py \"$full_qasm_file\" \"$COMPILER\" \"$RESULTS_FOLDER\""
+
+eval $command
